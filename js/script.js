3104f156ac856bd21dfb80541b43cf45ebcc0e59
Chrisvaldar <christophervalensio1@gmail.com>
2026-01-13 10:18:16 +1100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//Navigation Bar
+// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not
+function ariaExpanded() {
+	const csUL = document.querySelector('#cs-expanded');
+	const csExpanded = csUL.getAttribute('aria-expanded');
+
+	if (csExpanded === 'false') {
+		csUL.setAttribute('aria-expanded', 'true');
+	} else {
+		csUL.setAttribute('aria-expanded', 'false');
+	}
+}                               
+
+//Booking Form
+const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
+        for (const item of faqItems) {
+            const onClick = () => {
+            item.classList.toggle('active')
+        }
+        item.addEventListener('click', onClick)
+        }
+
+const galleryImages = document.querySelectorAll(".cs-image");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+galleryImages.forEach(function (img) {
+    img.addEventListener("click", function () {
+        lightboxImg.src = img.src;
+        lightbox.classList.add("active");
+    });
+});
+
+lightbox.addEventListener("click", function () {
+    lightbox.classList.remove("active");
+});
